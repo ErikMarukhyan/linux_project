931a7cc03fa10b757e1ee2140e0c2826fcc582c9
ErikMarukhyan <90900123+ErikMarukhyan@users.noreply.github.com>
2021-09-17 13:45:42 +0400

    Add files via upload

---
+++ build/CMakeFiles/crawler.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/diav3l/Desktop/webbrowser/indexer.cpp" "/home/diav3l/Desktop/webbrowser/build/CMakeFiles/crawler.dir/indexer.cpp.o"
+  "/home/diav3l/Desktop/webbrowser/linkqueue.cpp" "/home/diav3l/Desktop/webbrowser/build/CMakeFiles/crawler.dir/linkqueue.cpp.o"
+  "/home/diav3l/Desktop/webbrowser/repo_in_memory.cpp" "/home/diav3l/Desktop/webbrowser/build/CMakeFiles/crawler.dir/repo_in_memory.cpp.o"
+  "/home/diav3l/Desktop/webbrowser/server.cpp" "/home/diav3l/Desktop/webbrowser/build/CMakeFiles/crawler.dir/server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "POCO_NO_AUTOMATIC_LIBS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/crawler.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crawler.dir/indexer.cpp.o"
+  "CMakeFiles/crawler.dir/linkqueue.cpp.o"
+  "CMakeFiles/crawler.dir/repo_in_memory.cpp.o"
+  "CMakeFiles/crawler.dir/server.cpp.o"
+  "crawler"
+  "crawler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/crawler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
